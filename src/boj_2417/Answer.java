8399e095b8c9be0e671b9ea3613745802b37eb00
ttasjwi <ttasjwi920@gmail.com>
2021-11-07 20:27:46 +0900

    백준 답안 제출용

--- src/boj_2417/Answer.java
+++ src/boj_2417/Answer.java
@@ -9,10 +9,8 @@ public class Answer {
         final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         final long n = Long.parseLong(br.readLine());
         br.close();
-        final double rootN = Math.pow(n, 0.5); // 양의 제곱근
-        final long q  = (rootN - (long)rootN == 0) // 소수부분이 0인지 여부를 판단
-                ? (long)rootN // 소수부분이 0이면 올리지 않아도 됨
-                : (long)Math.ceil(rootN); // 소수부분이 0이 아니면 올림처리
+        final double sqrtN = Math.pow(n, 0.5);
+        final long q = (long) Math.ceil(sqrtN); // 양의 제곱근값의 올림값
         System.out.println(q);
     }
 }
