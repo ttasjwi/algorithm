179fe05689d959e796195251ec193be4499349ae
ttasjwi <ttasjwi920@gmail.com>
2021-12-31 10:21:02 +0900

    [Baekjoon Online Judge] [Bronze 5] [1000] A+B - 디렉토리 이동

---
+++ Online Judge/Baekjoon Online Judge/# 01. Bronze Ⅴ/# 01000. A+B/README.md
@@ -0,0 +1,21 @@
+# \[BOJ 01000\] A+B
+
+난이도 : Bronze5
+
+입력값 두개를 받고 합산한 결과값 출력하기(단순 입출력)
+문제 : <a href="https://www.acmicpc.net/problem/1000" target="_blank"> [링크]</a>
+
+---  
+
+## Main.java : 백준 제출 답안
+
+- 소스 코드 : <a href="src/Main.java" target="_blank">gitHub</a>
+- 단순히 입력받고, 합산하여 출력하는 문제
+
+---
+
+## Review
+- java로 알고리즘 문제 풀 때는 `Scanner`보다는 `BufferedReader`가 속도면에서 압도적이라서 BufferedReader를 쓰는게 좋다.
+  - 심지어 일부 문제는 `Scanner`를 사용했을 때 시간 초과가 나기도 함...
+
+---
--- Online Judge/Baekjoon Online Judge/# 01. Bronze Ⅴ/# 01000. A+B/src/Main.java
+++ Online Judge/Baekjoon Online Judge/# 01. Bronze Ⅴ/# 01000. A+B/src/Main.java
@@ -1,11 +1,10 @@
-package boj_1000;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Answer {
+public class Main {
 	
 	public static void main(String[] args) throws IOException {
 
@@ -13,10 +12,10 @@ public class Answer {
 		String input = br.readLine();
 		br.close();
 
-		StringTokenizer st = new StringTokenizer(input, " ");
+		StringTokenizer st = new StringTokenizer(input, " "); // " "을 구분자로 하여 입력받음
 
-		int a = Integer.parseInt(st.nextToken());
+		int a = Integer.parseInt(st.nextToken()); // 값 두개를 입력받는다
 		int b = Integer.parseInt(st.nextToken());
-		System.out.println(a+b);
+		System.out.println(a+b); // 합을 출력한다
 	}
 }
