bc2c4d29eabdb3cbe7416f475ec419b0fdab6822
ttasjwi <ttasjwi920@gmail.com>
2022-01-02 12:20:18 +0900

    [Baekjoon Online Judge] [Bronze 3] [2445] 별 찍기 - 8

---
+++ Online Judge/Baekjoon Online Judge/# 03. Bronze Ⅲ/# 02445. 별 찍기 - 8/src/Main.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        br.close();
+
+        StringBuilder sb = new StringBuilder();
+
+        int starCount = 1;
+
+        while(starCount < N) {
+            sb.append("*".repeat(starCount))
+                    .append(" ".repeat(2 * (N - starCount)))
+                    .append("*".repeat(starCount))
+                    .append('\n');
+            starCount++; // 별의 갯수 증가
+        }
+        while(starCount > 0) {
+            sb.append("*".repeat(starCount))
+                    .append(" ".repeat(2 * (N - starCount)))
+                    .append("*".repeat(starCount))
+                    .append('\n');
+            starCount--; // 별의 갯수 감소
+        }
+
+        System.out.print(sb);
+
+
+    }
+}
