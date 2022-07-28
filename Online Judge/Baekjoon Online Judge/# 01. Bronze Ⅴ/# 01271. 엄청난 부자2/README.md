b7dc2056d1427e1ff0fe88908537bce60df13ce1
ttasjwi <ttasjwi920@gmail.com>
2022-07-28 09:43:15 +0900

    [Baekjoon Online Judge] [Bronze 5] [01271] 엄청난 부자2

---
+++ Online Judge/Baekjoon Online Judge/# 01. Bronze Ⅴ/# 01271. 엄청난 부자2/README.md
@@ -0,0 +1,19 @@
+
+# \[BOJ 01271\] 엄청난 부자2
+
+- 난이도 : Bronze5
+- 몫, 나머지 구하라는 문제 + BigInteger
+- 문제: <a href="https://www.acmicpc.net/problem/1271" target="_blank"> [링크]</a>
+
+---
+
+## 주의점
+
+주어진 조건값들이 int, long의 범위를 벗어나므로 BigInteger를 사용해야함
+
+---
+
+## 학습거리
+- BigInteger, BigDecimal
+
+---
---
+++ Online Judge/Baekjoon Online Judge/# 01. Bronze Ⅴ/# 01271. 엄청난 부자2/src/Main.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+
+        BigInteger n = new BigInteger(st.nextToken());
+        BigInteger m = new BigInteger(st.nextToken());
+
+        BigInteger answer1 = n.divide(m);
+        BigInteger answer2 = n.mod(m);
+
+        StringBuilder sb = new StringBuilder();
+        sb.append(answer1).append('\n').append(answer2);
+        System.out.println(sb);
+    }
+
+}
