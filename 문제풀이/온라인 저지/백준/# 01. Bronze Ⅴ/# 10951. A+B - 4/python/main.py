688c395b8ffc0468f6c9c75a98eb0392b63e57a7
ttasjwi <ttasjwi920@gmail.com>
2023-01-25 11:14:43 +0900

    [BOJ] [Bronze 5] [10951] A+B - 4

---
+++ 문제풀이/온라인 저지/백준/# 01. Bronze Ⅴ/# 10951. A+B - 4/python/main.py
@@ -0,0 +1,18 @@
+import io, os, sys
+
+inputs = io.BytesIO(os.read(0, os.fstat(0).st_size)).readlines
+print = sys.stdout.write
+
+
+def main():
+    answer = '\n'.join(str(get_sum(line)) for line in inputs())
+    print(answer)
+
+
+def get_sum(line: str):
+    a, b = map(int, line.split())
+    return a + b
+
+
+if __name__ == '__main__':
+    main()
