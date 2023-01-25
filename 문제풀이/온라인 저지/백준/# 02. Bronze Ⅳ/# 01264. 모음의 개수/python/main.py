a609458945132e0def48848a742e2e10e9dcc495
ttasjwi <ttasjwi920@gmail.com>
2023-01-25 13:16:12 +0900

    [BOJ] [Bronze 4] [01264] 모음의 개수

--- 문제풀이/온라인 저지/백준/# 02. Bronze Ⅳ/# 01264. 모음의 개수/python/main.py
+++ 문제풀이/온라인 저지/백준/# 02. Bronze Ⅳ/# 01264. 모음의 개수/python/main.py
@@ -1,20 +1,3 @@
-# 문제
-- 플랫폼 : 백준
-- 번호 : 01264
-- 제목 : 모음의 개수
-- 난이도 : Bronze 4
-- 각 줄마다 모음의 개수를 세서 출력
-- 문제 : <a href="https://www.acmicpc.net/problem/1264" target="_blank">링크</a>
-
----
-
-# 필요 알고리즘
-- 없음
-
----
-
-# 풀이
-```python
 import io, os, sys
 
 print = sys.stdout.write
@@ -37,6 +20,3 @@ def calculate_count(line):
 
 if __name__ == '__main__':
     main()
-```
-
----
