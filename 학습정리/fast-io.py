ca45f9e1e412b24c94d66b7abb592a82b2feeb64
ttasjwi <ttasjwi920@gmail.com>
2023-01-25 11:33:05 +0900

    fast-io 수정

--- 학습정리/fast-io.py
+++ 학습정리/fast-io.py
@@ -1,6 +1,13 @@
 import io, os, sys
 
+# 라인울 문자열로
 input = io.BytesIO(os.read(0, os.fstat(0).st_size)).readline
-# inputs = io.BytesIO(os.read(0, os.fstat(0).st_size))
 
+# 라인들을 리스트로
+input_lines = io.BytesIO(os.read(0, os.fstat(0).st_size)).readlines
+
+# 모든 문자열 통채로(decode 필요)
+input_all = io.BytesIO(os.read(0, os.fstat(0).st_size)).read
+
+# 출력
 print = sys.stdout.write
