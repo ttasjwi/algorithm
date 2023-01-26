df94fe58393fb7f1c791627c560ab31245da106a
ttasjwi <ttasjwi920@gmail.com>
2023-01-26 18:44:35 +0900

    [BOJ] [Bronze 3] [10992] 별 찍기 - 17

--- 문제풀이/온라인 저지/백준/# 03. Bronze Ⅲ/# 10992. 별 찍기 - 17/python/main.py
+++ 문제풀이/온라인 저지/백준/# 03. Bronze Ⅲ/# 10992. 별 찍기 - 17/python/main.py
@@ -1,20 +1,3 @@
-# 문제
-- 플랫폼 : 백준
-- 번호 : 10992
-- 제목 : 별 찍기 - 17
-- 난이도 : Bronze 3
-- 예제를 보고 규칙을 유추한 뒤에 별을 찍기
-- 문제 : <a href="https://www.acmicpc.net/problem/10992" target="_blank">링크</a>
-
----
-
-# 필요 알고리즘
-- 없음
-
----
-
-# 풀이
-```python
 def main():
     n = int(input())
     answer = '\n'.join(make_line(i, n) for i in range(1, n + 1))
@@ -32,6 +15,3 @@ def make_line(i, n):
 
 if __name__ == '__main__':
     main()
-```
-
----
