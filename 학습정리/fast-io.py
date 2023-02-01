4c5a00ea9252b20b682ffe74fb8945958e93baea
ttasjwi <ttasjwi920@gmail.com>
2023-02-02 07:15:56 +0900

    fast-io 수정

--- 학습정리/fast-io.py
+++ 학습정리/fast-io.py
@@ -1,11 +1,6 @@
 import io, os, sys
 
-# 백준용 : read, readline, readlines
-src = io.BytesIO(os.read(0, os.fstat(0).st_size))
-# 로컬 테스트
-src = open('input.txt', 'rb')
-
-lines = src.read().decode().rstrip().split(sep='\n')
-
-# 출력
 print = sys.stdout.write
+
+lines = io.BytesIO(os.read(0, os.fstat(0).st_size)).read().decode().rstrip().split(sep='\n')
+lines = open('input.txt', 'rb').read().decode().rstrip().split(sep='\n')
