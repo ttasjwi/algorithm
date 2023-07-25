9bf17f75d5c2408cb0d8d0b15bd82f8ba4f74068
ttasjwi <ttasjwi920@gmail.com>
2023-07-25 23:52:51 +0900

    kotlin fast io

---
+++ 학습정리/fast-io.kts
@@ -0,0 +1,27 @@
+// 음수 포함o 정수 입력
+private fun readInt(): Int {
+    var value = 0
+    var negative = false
+    var input: Int
+    while (true) {
+        input = System.`in`.read()
+        when (input) {
+            10, 32 -> return if (negative) -value else value
+            45 -> negative = true
+            else -> value = value * 10 + (input - 48)
+        }
+    }
+}
+
+// 음수 포함x 정수 입력
+private fun readInt(): Int {
+    var value = 0
+    var input: Int
+    while (true) {
+        input = System.`in`.read()
+        when (input) {
+            10, 32 -> return value // 공백이나 개행자
+            else -> value = value * 10 + (input - 48)
+        }
+    }
+}
