e7537c8f2907ddea0a435e420d95d5424ceb7a02
ttasjwi <ttasjwi920@gmail.com>
2023-08-07 14:15:17 +0900

    fast io

--- 학습정리/fast-io.kts
+++ 학습정리/fast-io.kts
@@ -20,8 +20,28 @@ private fun readInt(): Int {
     while (true) {
         input = System.`in`.read()
         when (input) {
-            10, 32 -> return value // 공백이나 개행자
+            10, 32 -> return value
             else -> value = value * 10 + (input - 48)
         }
     }
 }
+
+
+// System.`in`.read(B,0,S) 로 읽기
+const val S=10500007
+private val B=ByteArray(S)
+private var p=0
+fun i():Int{
+    var v=0
+    var c=B[p++]
+    do v=v*10+c-48 while(B[p++].also{c=it}>47)
+    return v
+}
+fun s():String{
+    val f=p; var l=0
+    while(B[p++]>64)l++
+    return String(B,f,l)
+}
+
+fun main()=System.out.bufferedWriter().run{
+    System.`in`.read(B,0,S)
