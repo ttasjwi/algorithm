3e2e603685e4662f4951823e9d0d2edf2e04e6bd
ttasjwi <ttasjwi920@gmail.com>
2023-08-09 11:57:36 +0900

    fast io

--- 학습정리/fast-io.kts
+++ 학습정리/fast-io.kts
@@ -26,26 +26,43 @@ private fun readInt(): Int {
     }
 }
 
+private const val S = 65536
+private val iS = java.io.DataInputStream(System.`in`)
+private val b = ByteArray(S)
+private var c = 0
+private var l = 0
+private val rsb = StringBuilder()
+private val wsb = StringBuilder()
 
-const val S=10500007
-private val B=ByteArray(S)
-private var p=0
-fun i():Int{
-    var v=0
-    var c=B[p++]
-    do v=v*10+c-48 while(B[p++].also{c=it}>47)
+private fun r(): Byte {
+    if (c == l) {
+        l = iS.read(b, 0.also { c = it }, S)
+        if (l == -1) b[0] = -1
+    }
+    return b[c++]
+}
+
+private fun i(): Int {
+    var v = 0
+    var c = r()
+    do v = v * 10 + c - 48 while (r().also { c = it } > 47)
     return v
 }
-private val rsb = StringBuilder()
-fun s(): String {
-    var c = B[p++]
-    do {
-        rsb.append(c.toChar())
-    } while (B[p++].also { c = it } > 47)
+
+private fun i(): Int {
+    var v = 0
+    var c = r()
+    var n = c.toInt() == 45
+    c = if (n) c else r()
+    do { v = v * 10 + c - 48 } while (r().also { c = it } > 47)
+    return if (n) -v else v
+}
+
+
+private fun s(): String {
+    var c = r()
+    do rsb.append(c.toChar()) while (r().also { c = it } > 32)
     val s = rsb.toString()
     rsb.setLength(0)
     return s
 }
-
-fun main()=System.out.bufferedWriter().run{
-    System.`in`.read(B,0,S)
