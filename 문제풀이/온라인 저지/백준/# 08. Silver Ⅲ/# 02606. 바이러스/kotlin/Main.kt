392251ba79bf6277e1c12fc2989a6271f27d55f9
ttasjwi <ttasjwi920@gmail.com>
2023-09-19 11:36:19 +0900

    [BOJ] [Silver 3] [02606] 바이러스

--- 문제풀이/온라인 저지/백준/# 08. Silver Ⅲ/# 02606. 바이러스/kotlin/Main.kt
+++ 문제풀이/온라인 저지/백준/# 08. Silver Ⅲ/# 02606. 바이러스/kotlin/Main.kt
@@ -1,21 +1,3 @@
-# 문제
-- 플랫폼 : 백준
-- 번호 : 02606
-- 제목 : 바이러스
-- 난이도 : Silver 3
-- 컴퓨터의 수와 네트워크 상에서 서로 연결되어 있는 정보가 주어질 때, 1번 컴퓨터를 통해 웜 바이러스에 걸리게 되는 컴퓨터의 수를
-출력하는 프로그램을 작성
-- 문제 : <a href="https://www.acmicpc.net/problem/2606" target="_blank">링크</a>
-
----
-
-# 필요 알고리즘
-- DFS
-- BFS
----
-
-# 풀이
-```kotlin
 fun main() {
     val n = i()
     val check = BooleanArray(n+1)
@@ -60,8 +42,26 @@ fun main() {
     }
     print(answer)
 }
-```
-- 배열기반 원형큐로 BFS를 구현했다.
-- 큐에 삽입하기 전에, 도착 지점을 감염시키고 삽입한다.
 
----
+
+private const val S = 65536
+private val iS = java.io.DataInputStream(System.`in`)
+private val b = ByteArray(S)
+private var c = 0
+private var l = 0
+
+private fun i(): Int {
+    var v = 0
+    var c = r()
+    do v = v * 10 + c - 48 while (r().also { c = it } > 47)
+    return v
+}
+
+
+private fun r(): Byte {
+    if (c == l) {
+        l = iS.read(b, 0.also { c = it }, S)
+        if (l == -1) b[0] = -1
+    }
+    return b[c++]
+}
