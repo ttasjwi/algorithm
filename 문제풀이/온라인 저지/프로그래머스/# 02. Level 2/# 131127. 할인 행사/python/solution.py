f9e1255c3d45041cbfce4ab7cad61b78d999aa6a
ttasjwi <ttasjwi920@gmail.com>
2025-02-11 13:27:02 +0900

    [Programmers] [Level 2] [131127] 할인 행사

--- 문제풀이/온라인 저지/프로그래머스/# 02. Level 2/# 131127. 할인 행사/python/solution.py
+++ 문제풀이/온라인 저지/프로그래머스/# 02. Level 2/# 131127. 할인 행사/python/solution.py
@@ -1,19 +1,3 @@
-# 문제
-- 플랫폼 : 프로그래머스
-- 번호 : 131127
-- 제목 : 할인 행사
-- 난이도 : Level 2
-- 문제 : <a href="https://school.programmers.co.kr/learn/courses/30/lessons/131127" target="_blank">링크</a>
-
----
-
-# 필요 지식
-- 해시
-
----
-
-# 풀이
-```python
 def solution(want, number, discount):
     answer = 0
     want_counter = {} # 희망 구매 목록
@@ -40,13 +24,3 @@ def solution(want, number, discount):
         if day >= 9 and want_counter == daily_counter:
             answer += 1
     return answer
-```
-- want_counter : 내가 원하는 갯수를 가리키는 카운터
-- daily_counter : 최근 10일간, 각 제품 할인일수 누적 카운터
-- 카운터를 비교하기 위해서는 각 카운터가 가진 제품군의 종류를 맞춘다.
-- 날짜가 경과할 때마다 다음 작업을 수행한다.
-  - 오늘 할인 품목을 daily_counter 에서 찾아 1 증가
-  - 10일 전 할인 품목을 daily_counter 에서 찾아 1 감소
-  - daily_counter 와 want_counter 비교 후 같으면 1 증가
-
----
