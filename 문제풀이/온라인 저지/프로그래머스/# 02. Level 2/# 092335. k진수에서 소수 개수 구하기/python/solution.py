b95b8eecc3cc17dc2a74237c44326753f764bfba
ttasjwi <ttasjwi920@gmail.com>
2025-02-21 10:57:09 +0900

    [Programmers] [Level 2] [092335] k진수에서 소수 개수 구하기

--- 문제풀이/온라인 저지/프로그래머스/# 02. Level 2/# 092335. k진수에서 소수 개수 구하기/python/solution.py
+++ 문제풀이/온라인 저지/프로그래머스/# 02. Level 2/# 092335. k진수에서 소수 개수 구하기/python/solution.py
@@ -1,21 +1,3 @@
-# 문제
-- 플랫폼 : 프로그래머스
-- 번호 : 092335
-- 제목 : k진수에서 소수 개수 구하기
-- 난이도 : Level 2
-- 문제 : <a href="https://school.programmers.co.kr/learn/courses/30/lessons/92335" target="_blank">링크</a>
-
----
-
-# 필요 지식
-- 문자열 처리
-- 수학
-- 스택
-
----
-
-# 풀이
-```python
 from math import sqrt
 
 def solution(n, k):
@@ -51,10 +33,3 @@ def is_prime_number(n):
         if n % i == 0:
             return False
     return True
-```
-- 숫자 변환
-  - n 을 k 로 나눠가면서, 나머지를 stack에 쌓기
-  - stack 을 역으로 꺼내면서, 0이 나오기 전까지 숫자를 계속 십진수로 변환하고, 결과 배열에 저장
-- 위 과정을 통해 만들어진 배열을 순회하면서, 소수의 갯수 세기
-
----
