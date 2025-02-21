23eaf3b4f59b9aca741168a1d9c9ec2b0bb858d8
ttasjwi <ttasjwi920@gmail.com>
2025-02-21 13:43:52 +0900

    [Programmers] [Level 2] [154539] 뒤에 있는 큰 수 찾기

--- 문제풀이/온라인 저지/프로그래머스/# 02. Level 2/# 154539. 뒤에 있는 큰 수 찾기/python/solution.py
+++ 문제풀이/온라인 저지/프로그래머스/# 02. Level 2/# 154539. 뒤에 있는 큰 수 찾기/python/solution.py
@@ -1,19 +1,3 @@
-# 문제
-- 플랫폼 : 프로그래머스
-- 번호 : 154539
-- 제목 : 뒤에 있는 큰 수 찾기
-- 난이도 : Level 2
-- 문제 : <a href="https://school.programmers.co.kr/learn/courses/30/lessons/154539" target="_blank">링크</a>
-
----
-
-# 필요 지식
-- 스택
-
----
-
-# 풀이
-```python
 def solution(numbers):
     stack = []
     result = [-1] * len(numbers)
@@ -22,11 +6,10 @@ def solution(numbers):
         # 스택에 요소가 있고, 스택의 마지막 요소가 현재 값보다 작거나 같으면 가치가 없으므로 전부 버림
         while stack and stack[-1] <= numbers[i]:
             stack.pop()
-            
+
         # 스택에 요소가 남아있다면, 스택에 남아있는 수는 가장 가까운 가장 큰 수가 됨
         if stack:
             result[i] = stack[-1]
         # 스택에 현재 값을 삽입
         stack.append(numbers[i])
     return result
-```
