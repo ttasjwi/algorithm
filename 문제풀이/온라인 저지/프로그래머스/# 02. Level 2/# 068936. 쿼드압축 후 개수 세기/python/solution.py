ead4f5086afe506426d8db097d8940d0a87eacbd
ttasjwi <ttasjwi920@gmail.com>
2025-02-26 14:03:45 +0900

    [Programmers] [Level 2] [068936] 쿼드압축 후 개수 세기

--- 문제풀이/온라인 저지/프로그래머스/# 02. Level 2/# 068936. 쿼드압축 후 개수 세기/python/solution.py
+++ 문제풀이/온라인 저지/프로그래머스/# 02. Level 2/# 068936. 쿼드압축 후 개수 세기/python/solution.py
@@ -1,19 +1,3 @@
-# 문제
-- 플랫폼 : 프로그래머스
-- 번호 : 068936
-- 제목 : 쿼드압축 후 개수 세기
-- 난이도 : Level 2
-- 문제 : <a href="https://school.programmers.co.kr/learn/courses/30/lessons/68936" target="_blank">링크</a>
-
----
-
-# 필요 지식
-- 분할 정복
-
----
-
-# 풀이
-```python
 def solution(arr):
     answer = [0, 0]
 
@@ -37,8 +21,3 @@ def solution(arr):
 
     compress(0, 0, len(arr))
     return answer
-```
-- 배열 전체를 순회하고 같지 않으면 다시 사등분해서 재귀하는 방식은 중복 탐색을 하는 문제가 발생한다.
-- 먼저 재귀적으로 사등분한 뒤, 사등분 영역이 모두 같으면 합쳐나가는 식으로 처리하면 중복 탐색을 하지 않게 된다.
-
----
