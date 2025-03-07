7c8364c0ea7a173d154af38fe053dd22550ee167
ttasjwi <ttasjwi920@gmail.com>
2025-03-07 10:42:05 +0900

    [Programmers] [Level 2] [072411] 메뉴 리뉴얼

--- 문제풀이/온라인 저지/프로그래머스/# 02. Level 2/# 072411. 메뉴 리뉴얼/python/solution.py
+++ 문제풀이/온라인 저지/프로그래머스/# 02. Level 2/# 072411. 메뉴 리뉴얼/python/solution.py
@@ -1,31 +1,22 @@
-# 문제
-- 플랫폼 : 프로그래머스
-- 번호 : 072411
-- 제목 : 메뉴 리뉴얼
-- 난이도 : Level 2
-- 문제 : <a href="https://school.programmers.co.kr/learn/courses/30/lessons/72411" target="_blank">링크</a>
-
----
-
-# 필요 지식
-- 정렬
-
----
-
-# 풀이
-```python
 from itertools import combinations
 from collections import Counter
 
 def solution(orders, course):
     result = []
     for r in course:
+
+        # r 개 조합 문자열 -> 사전식 정렬 문자열을 -> counter 에 삽입 -> 가장 흔하게 등장한 순으로
         counter = Counter([''.join(sorted(tuple)) for order in orders for tuple in combinations(order, r)]).most_common()
+
+        # 가장 많이 등장한 횟수가 1 이면 result 에 삽입 x
         if not counter or counter[0][1] == 1:
             continue
+
+        # 등장횟수가 최댓값이 아닐 떄 중단하고, 최댓값인 동안 result에 계속 삽입
         for k, v in counter:
             if v != counter[0][1]:
                 break
             result.append(k)
+
+    # result 를 다시 사전순 정렬하여 반환
     return sorted(result)
-```
