03176a7ba165cb3277c9ac5dd22746cae29097d4
ttasjwi <ttasjwi920@gmail.com>
2025-03-08 10:20:00 +0900

    [Programmers] [Level 2] [154540] 무인도 여행

--- 문제풀이/온라인 저지/프로그래머스/# 02. Level 2/# 154540. 무인도 여행/python/solution.py
+++ 문제풀이/온라인 저지/프로그래머스/# 02. Level 2/# 154540. 무인도 여행/python/solution.py
@@ -1,46 +1,29 @@
-# 문제
-- 플랫폼 : 프로그래머스
-- 번호 : 154540
-- 제목 : 무인도 여행
-- 난이도 : Level 2
-- 문제 : <a href="https://school.programmers.co.kr/learn/courses/30/lessons/154540" target="_blank">링크</a>
+directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
 
----
-
-# 필요 지식
-- DFS
-
----
-
-# 풀이
-```python
-directions = [(1, 0), (0,1), (-1, 0), (0, -1)]
 
 def solution(maps):
     maps = [list(map) for map in maps]
     result = []
-    
-    def dfs(r,c, maps):
-        stack = [(r,c)]
+
+    def dfs(r, c, maps):
+        stack = [(r, c)]
         day = int(maps[r][c])
         maps[r][c] = 'X'
-        
+
         while stack:
             cr, cc = stack.pop()
             for dr, dc in directions:
                 nr, nc = cr + dr, cc + dc
-                
-                if 0<=nr<len(maps) and 0<=nc<len(maps[0]) and maps[nr][nc] != 'X':
+
+                if 0 <= nr < len(maps) and 0 <= nc < len(maps[0]) and maps[nr][nc] != 'X':
                     day += int(maps[nr][nc])
                     maps[nr][nc] = 'X'
                     stack.append((nr, nc))
         return day
-    
-    
+
     for r in range(len(maps)):
         for c in range(len(maps[0])):
             if maps[r][c] != 'X':
-                day = dfs(r,c, maps)
+                day = dfs(r, c, maps)
                 result.append(day)
     return [-1] if not result else sorted(result)
-```
