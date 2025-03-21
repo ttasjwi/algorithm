79f2ef9b0786f5fa64443dce65efeb35a599354b
ttasjwi <ttasjwi920@gmail.com>
2025-03-21 10:14:32 +0900

    [BOJ] [Silver 3] [02606] 바이러스

--- 문제풀이/온라인 저지/백준/# 08. Silver Ⅲ/# 02606. 바이러스/python/main.py
+++ 문제풀이/온라인 저지/백준/# 08. Silver Ⅲ/# 02606. 바이러스/python/main.py
@@ -1,20 +1,3 @@
-# 문제
-- 플랫폼 : 백준
-- 번호 : 02606
-- 제목 : 바이러스
-- 난이도 : Silver 3
-- 문제 : <a href="https://www.acmicpc.net/problem/2606" target="_blank">링크</a>
-
----
-
-# 필요 지식
-- DFS
-- BFS
-
----
-
-# 풀이
-```python
 import sys
 
 input = sys.stdin.readline
@@ -39,4 +22,3 @@ while stack:
             answer += 1
             stack.append(near_node)
 print(answer)
-```
