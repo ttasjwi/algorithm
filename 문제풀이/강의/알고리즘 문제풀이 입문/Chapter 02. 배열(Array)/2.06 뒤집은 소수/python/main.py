ff8cfcfe447ab9bf62194f482f4fd880c2143927
ttasjwi <ttasjwi920@gmail.com>
2025-08-28 17:30:25 +0900

    [강의] 2.06 뒤집은 소수

--- 문제풀이/강의/알고리즘 문제풀이 입문/Chapter 02. 배열(Array)/2.06 뒤집은 소수/python/main.py
+++ 문제풀이/강의/알고리즘 문제풀이 입문/Chapter 02. 배열(Array)/2.06 뒤집은 소수/python/main.py
@@ -2,12 +2,21 @@ from math import sqrt
 
 input()
 nums = [int(s[::-1]) for s in input().split()]
-mx = max(nums)
+max_number = max(nums) # 입력값 중 제일 큰수 구하기
 
-check = [True, True] + [False] * (mx - 1)
-for i in range(2, int(sqrt(mx)) + 1):
-    if not check[i]:
-        for j in range(2 * i, mx + 1, i):
-            check[j] = True
+# 에라토스테네스의 체 사용 -> 소수 찾기
+is_prime = [False, False] + [True] * (max_number - 1)
+for i in range(2, int(sqrt(max_number)) + 1):
+    if is_prime[i]:
+        for j in range(2 * i, max_number + 1, i):
+            is_prime[j] = False
 
-print(' '.join(str(num) for num in nums if not check[num]), end='')
+# 입력 받아서, 뒤집고, 소수이면 result에 담기
+result = []
+for num in input().split():
+    num = int(num[::-1])
+    if is_prime[num]:
+        result.append(num)
+
+# 출력
+print(*result, end='', sep=' ')
